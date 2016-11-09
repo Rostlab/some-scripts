6cc68f768828ba569a13c72775b4e804a29395b2
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-09 10:51:07 +0100

    add scripts

---
+++ gold_download.py
@@ -0,0 +1,119 @@
+# Author: Juan Miguel Cejuela (@juanmirocks)
+
+# Requires: Python 2
+
+help = """
+Download organism records from GOLD (Genomes Online Database,
+http://www.genomesonline.org/cgi-bin/GOLD/index.cgi)
+
+Use:     %s [--help|--lowercase] (goldcard)*
+
+Example: %s Gc00001 Gc00002 ...
+
+
+The script will download all records to the **current directory**. Each record
+is downloaded as an xml file. The syntax is straightforward. For example,
+compare the xml output for Gc00001 with
+http://www.genomesonline.org/cgi-bin/GOLD/GOLDCards.cgi?goldstamp=Gc00001
+
+Note: GOLD does not provide xml records. This script fetches the html from the
+webpage and tries its best to parse it. The html itself is malformed so the
+parsing can go wrong. Report problems to @jmcejuela
+
+OPTIONS
+    --lowercase    down case all field names and values. This can be useful
+                   to normalize records that perhaps use a different case
+"""
+
+def printhelp():
+    print help  % (sys.argv[0], sys.argv[0])
+    sys.exit()
+
+import requests, re, codecs, sys
+from bs4 import BeautifulSoup
+from bs4 import element
+from lxml import etree
+
+def record_url(goldcard): return "http://www.genomesonline.org/cgi-bin/GOLD/GOLDCards.cgi?goldstamp=" + goldcard
+
+def case(x): return x
+
+def record_html_soup(recordUrl):
+    req = requests.get(recordUrl)
+    soup = BeautifulSoup(req.content)
+    return soup
+
+def parse_table(table):
+    section = etree.Element("section")
+    section.set("name", case(table.thead.tr.th.string.strip()))
+    for row in table('tr')[1:]:
+        field = etree.Element("field")
+        field.set("name", case(row.select('> th')[1].get_text().strip()))
+        rootValueNodes = row.td
+        if rootValueNodes:
+            values = []
+            for child in rootValueNodes.children:
+                if isinstance(child, element.NavigableString):
+                    c = child.strip()
+                else:
+                    c = child.get_text().strip()
+                if c:
+                    values.append(c)
+            if values:
+                field.set("value", case(", ".join(values)))
+
+        mgi = row.th.get_text().strip()
+        if mgi:
+            field.set("migs", case(mgi))
+        section.append(field)
+    return section
+
+def create_ElementTree(goldcard):
+    url = record_url(goldcard)
+    soup = record_html_soup(url)
+    # print soup.prettify() #for debugging
+
+    root = etree.Element("organism")
+    etree.SubElement(root, "goldcard").text = goldcard
+    etree.SubElement(root, "name").text = soup.h2.string
+    etree.SubElement(root, "url").text = url
+    etree.SubElement(root, "mci").text = soup.h4.string.partition(")=")[2]
+    sections = etree.SubElement(root, "sections")
+
+    for table in soup.find_all('table'):
+        sections.append(parse_table(table))
+
+    return etree.ElementTree(root)
+
+def download_record(goldcard):
+    xml = create_ElementTree(goldcard)
+    filename = goldcard + ".xml"
+    file = codecs.open(filename, "w", "utf-8")
+    xml.write(file, pretty_print=True)
+    file.close()
+    return filename
+
+###############################################################################
+
+if len(sys.argv) <= 1:
+    printhelp()
+elif sys.argv[1].startswith("--"):
+    flag = sys.argv[1]
+    if flag == "--lowercase":
+        def case(x): return x.lower()
+    elif flag == "--help":
+        printhelp()
+    else:
+        print "wrong argument: " + flag
+        printhelp()
+    goldcards = sys.argv[2:]
+else:
+    goldcards = sys.argv[1:]
+
+for goldcard in goldcards:
+    print "gold card: " + goldcard
+    try:
+        download_record(goldcard)
+    except Exception as e:
+        print "   Oops! Couldn't download the record. Report this to @jmcejuela\nStack:\n\n"
+        print str(e)
---
+++ pmid2plos.py
@@ -0,0 +1,62 @@
+# Author: Juan Miguel Cejuela (@juanmirocks)
+
+# Requires: Python 3
+
+help = """
+Download PLOS (http://www.plos.org/) xml articles given a list of pmids (PubMed ids).
+The script assumes that the given pmids correspond to public PLOS articles.
+If that's not the case, the pmid is ignored but this is reported.
+
+Use:     %s (pmid)*
+
+Example  %s 21245904
+"""
+
+def printhelp():
+    print(help  % (sys.argv[0], sys.argv[0]))
+    sys.exit()
+
+import requests, re, codecs, sys
+
+def dxplos(pmid):
+    url = "http://www.ncbi.nlm.nih.gov/pubmed/" + pmid
+    req = requests.get(url)
+    match = re.search('href="(http://dx.plos.org/10.1371/[^"]+)"', req.text)
+    if match: return match.group(1)
+    else: None
+
+def urlplos(dxplos):
+    # http://dx.plos.org/10.1371/journal.pone.0028106
+    req = requests.get(dxplos)
+    return req.history[-1].headers['location']
+
+def urlxml(urlplos):
+    # http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0028106
+    m = re.search('(.*/)(info.*)', urlplos)
+    return m.group(1) + 'fetchObjectAttachment.action?uri=' + m.group(2) + '&representation=XML'
+
+def fetch(urlxml):
+    # http://www.plosone.org/article/fetchObjectAttachment.action?uri=info%3Adoi%2F10.1371%2Fjournal.pone.0028106&representation=XML
+    req = requests.get(urlxml)
+    req.encoding = "utf-8"
+    filename = re.search('filename="([^"]*)"', req.headers['content-disposition']).group(1)
+    file = codecs.open(filename, "w", "utf-8")
+    file.write(req.text)
+    file.close()
+    return filename
+
+###############################################################################
+
+if len(sys.argv) <= 1:
+    printhelp()
+
+for pmid in sys.argv[1:]:
+    print("PMID: " + pmid)
+    dx = dxplos(pmid)
+    if not dx:
+        print("  no plos article found!")
+    else:
+        try:
+            print("  " + fetch(urlxml(urlplos(dx))))
+        except:
+            print("  oops! couldn't fetch the xml (network problems on their/yours side?)")
